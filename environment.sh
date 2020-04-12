a86463b7b806284778d7c45e5b40bf6b1373683d
Ray Andrew <raydreww@gmail.com>
2020-04-12 14:35:31 +0700

    add environment

---
+++ environment.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Setting SCK OpenJDK11 development"
+
+export OBF_JAVA7_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+
+# path_dir="${PWD}/sources/build/linux-x86_64-normal-server-release/images/jdk/bin"
+
+echo "Setting succeed"
